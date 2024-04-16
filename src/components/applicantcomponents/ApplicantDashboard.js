b2c1df62e50a67d1a7691f22d4eecd43fe35ebe5
suhelshaik113 <gentlemancadetsuhel@gmail.com>
2024-04-16 14:33:34 +0530

    Applicant-dashboard-changes

--- src/components/applicantcomponents/ApplicantDashboard.js
+++ src/components/applicantcomponents/ApplicantDashboard.js
@@ -239,6 +239,7 @@ const ApplicantDashboard = () =>
     </div>
   </div>
   {/*New palet in application dashboard*/}
+  
   {/* <div className="box-icon1 wrap-counter" style={{ display: "flex", alignItems: "center" }} > */}
   <div className="box-icon1 wrap-counter" onClick={Buildresume} style={{ display: "flex", alignItems: "center", height: window.innerWidth >= 699 && window.innerWidth <= 1028 ? "250px" : "initial" }}>
 
