a0a993d51ee070ac8dc8ecc1db2562b9d6b44a23
suhelshaik113 <gentlemancadetsuhel@gmail.com>
2024-04-30 11:12:10 +0530

    Recruiter Forgat Password

--- src/components/recruitercomponents/RecruiterPostJob.js
+++ src/components/recruitercomponents/RecruiterPostJob.js
@@ -534,7 +534,7 @@ function RecruiterPostJob() {
               <div className="col-lg-12 col-md-12 ">
                 <div className="title-dashboard">
                 <BackButton />
-                  <div className="title-dash flex2">Post Job</div>
+                  <div className="title-dash flex2">Post a Job</div>
                 </div>
               </div>
             </div>
@@ -781,7 +781,7 @@ function RecruiterPostJob() {
                             id="skillsTypeahead"
                             labelKey={(option) => option.skillName}
                             multiple
-                            placeholder="*Skills"
+                            placeholder="Skills*"
                             options={filterOutSelectedSkills(skillsOptionsWithStructure, skillsRequired)}
                             onChange={(selectedSkills) => handleSkillsChange(selectedSkills)}
                             selected={skillsRequired}
