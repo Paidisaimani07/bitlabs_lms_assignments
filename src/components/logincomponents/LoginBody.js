30d4e28440f866c7d1cd4f825e3a85c478aea3e8
suhelshaik113 <gentlemancadetsuhel@gmail.com>
2024-06-06 12:48:28 +0530

    Changes in Signup,Login pages and forgot password of applicant.(TS-595 & TS-596)

--- src/components/logincomponents/LoginBody.js
+++ src/components/logincomponents/LoginBody.js
@@ -28,6 +28,7 @@ function LoginBody({ handleLogin }) {
   const [recruiterPasswordError, setRecruiterPasswordError] = useState('');
  const [candidateLoginInProgress, setCandidateLoginInProgress] = useState(false);
  const [registrationSuccessMessage, setRegistrationSuccessMessage] = useState('');
+ const [message, setMessage] = useState('Welcome Back');
  
  
 const login = useGoogleLogin({
@@ -466,6 +467,12 @@ const [candidatePasswordError1, setCandidatePasswordError1] = useState('');
  };
  const handleTabClick1 = (tab) => {
   setActiveTab(tab);
+  if (tab === 'Candidate') {
+    setMessage('Welcome Back');
+    console.log("login");
+  } else if (tab === 'Employer') {
+    setMessage('Create Account');
+  }
 };
 
 const getTabStyle = (tab) => ({
@@ -483,6 +490,15 @@ const getTabStyle = (tab) => ({
 
   return (
     <div>
+      <img
+        src={Backgroundimagemobile}
+        alt="Background"
+        className="responsive-image1"
+        style={{
+        //  height:"400px",
+        //  margintop:"10px"
+        }}
+      />
       <section className="account-section">
         <div className="tf-container">
           <div className="row">
@@ -493,26 +509,21 @@ const getTabStyle = (tab) => ({
                     Registration successful! Please log in to continue.
                   </div>
                 )}
-                 <img
-  src={Backgroundimagemobile}
-  alt="Background"
-  className="responsive-image1"
-  style={{
-    position: "fixed",
-    top: "0px",
-    left: "-20px",
-    paddingRight: "10px"
-  }}
-/>
-                {/* <h4><a href="/"><img src={logoCompany1} width="80px" height="80px"/></a> </h4> */}
-               
+                 
+      
+   
+    <div className="custom-div-style">
+      {message}
+    </div>
                 <div className="myComponent">
+                
       <div
   style={{
     ...getTabStyle('Candidate'),
     fontWeight: 'bold' // Add font weight inline
   }}
   onClick={() => handleTabClick1('Candidate')}
+  
 >
   Login
 </div>
