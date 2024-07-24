7a7ae9d673da24243fb9d91d852f77bfe025ae3d
MadarTekworks <madar.saheb@tekworks.in>
2024-07-24 17:41:11 +0530

    Completed mathed skills and unmatched skills and match percentage

--- src/components/applicantcomponents/ApplicantViewJob.js
+++ src/components/applicantcomponents/ApplicantViewJob.js
@@ -6,6 +6,8 @@ import { useLocation } from 'react-router-dom';
 import BackButton from '../common/BackButton';
 import Snackbar from '../common/Snackbar';
 import { apiUrl } from '../../services/ApplicantAPIService';
+import SemiCircleProgressBar from "react-progressbar-semicircle";
+
 
 const ApplicantViewJob = ({ selectedJobId }) => {
   const [jobDetails, setJobDetails] = useState(null);
@@ -16,6 +18,7 @@ const ApplicantViewJob = ({ selectedJobId }) => {
   const [snackbar, setSnackbar] = useState({ open: false, message: '', type: '', link: '', linkText: '' });
   const navigate = useNavigate();
   const jobId = new URLSearchParams(location.search).get('jobId') || selectedJobId;
+  
 
   const fetchJobDetails = async () => {
     try {
@@ -97,6 +100,9 @@ const ApplicantViewJob = ({ selectedJobId }) => {
     setSnackbar({ open: false, message: '', type: '', link: '', linkText: '' });
   };
 
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
   
 
   return (
@@ -119,6 +125,7 @@ const ApplicantViewJob = ({ selectedJobId }) => {
               <div className="content-tab">
                 <div className="inner">
                   <article className="job-article">
+                    
                     {jobDetails && (
                       <div className="top-content">
                         <div className="features-job style-2 stc-apply bg-white">
@@ -199,7 +206,101 @@ const ApplicantViewJob = ({ selectedJobId }) => {
                         </div>
                       </div>
                     )}
+                    
+                    
+                    
+                    {jobDetails && (
+                      <div className="features-job style-2 stc-apply bg-white">
+                        <div className="inner-content">
+                          <div className='row'>
+                          
+                          <div id="item_1" className="col-lg-8 col-md-12" style={{padding:'4px'}}>
 
+                          
+                          <h5 className='match-probability'>Skill Match Probability</h5>
+                          <p>The more the Probability, more are the chances to get hired.</p>
+                          </div>
+                         
+                          <div id="item_1" className="col-lg-4 col-md-12">
+                          <div className="right-aligned-content">
+                            <div className="progress-bar-container">
+                           
+                              <SemiCircleProgressBar percentage={jobDetails.matchPercentage} showPercentValue={false}  stroke="#F46F16" />
+                              <div className="progress-bar-value">{jobDetails.matchPercentage} %</div>
+                            </div>
+                            <div className="match">
+                              <p className="centered-text"><strong>{jobDetails.matchStatus}</strong></p>
+                            </div>
+                          </div>
+                          </div>
+                         </div>
+                        
+                         <div className="job-archive-footer">
+  <div className="job-footer-left1">
+    <ul className="job-tag">
+    {jobDetails.matchedSkills.map((skill, index) => (
+        <li key={index}>
+           <a 
+            href="javascript:void(0);"
+            style={{
+              backgroundColor: '#498C07', /* Green background color */
+              color: '#FFFF', /* White text color */
+              padding: '8px 12px', /* Padding around the text */
+              borderRadius: '50px', /* Rounded corners */
+              textDecoration: 'none', /* Remove underline */
+              display: 'inline-block', /* Ensure padding is applied */
+              transition: 'background-color 0.3s' /* Smooth transition for hover effect */
+              
+            }}
+          >
+            {skill.skillName}
+          </a>
+        </li>
+      ))}
+      {jobDetails.skillsRequired.map((skill, index) => (
+        <li key={index}>
+           <a 
+            href="javascript:void(0);"
+            style={{
+              backgroundColor: 'red', /* Green background color */
+              color: 'white', /* White text color */
+              padding: '8px 12px', /* Padding around the text */
+              height:'32',
+              borderRadius: '50px', /* Rounded corners */
+              textDecoration: 'none', /* Remove underline */
+              display: 'inline-block', /* Ensure padding is applied */
+              transition: 'background-color 0.3s' /* Smooth transition for hover effect */
+            }}
+          >
+             <span style={{
+              display: 'inline-block',
+              width: '18px',
+              height: '18px',
+              borderRadius: '50%',
+              border: '2px solid white', /* White border color */
+              backgroundColor: 'red', /* Circle background color */
+              color: 'white', /* Exclamation mark color */
+              textAlign: 'center',
+              lineHeight: '15px',
+              marginRight: '8px',
+              fontWeight: 'bold'
+            }}>!</span>
+             {capitalizeFirstLetter(skill.skillName)}
+          </a>
+        </li>
+      ))}
+     
+    </ul>
+  </div>
+</div>
+
+                         
+                        </div>
+                      </div>
+                    )}
+
+                   
+                  
                     {jobDetails && (
                       <div className="features-job style-2 stc-apply bg-white">
                         <div className="inner-content">
@@ -208,6 +309,7 @@ const ApplicantViewJob = ({ selectedJobId }) => {
                         </div>
                       </div>
                     )}
+                    
                   </article>
                 </div>
               </div>
