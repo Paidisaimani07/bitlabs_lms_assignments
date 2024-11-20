77fb42d096d76945977d20e7fa1ac40bada3f6c8
MadarTekworks <madar.saheb@tekworks.in>
2024-11-20 19:36:13 +0530

    completed TS-965

--- src/components/common/BackButton.js
+++ src/components/common/BackButton.js
@@ -3,19 +3,17 @@ import { Link, useNavigate} from 'react-router-dom';
 
 const BackButton = ({id,jobid}) => {
   const navigate = useNavigate();
+  
 
   const handleClick = (e) => {
     e.preventDefault();
       
        if (id && jobid) {
          const storageKey = `applicantData_${id}_${jobid}`;
-         localStorage.removeItem(storageKey);
-         console.log(`Removed ${storageKey} from localStorage`);
+         localStorage.removeItem(storageKey); 
        }
-  
-    navigate('/recruiter-allapplicants');
-  
-     console.log("all applicants");
+    // navigate('/recruiter-allapplicants');
+    navigate(-1); 
    };
    
   return (
