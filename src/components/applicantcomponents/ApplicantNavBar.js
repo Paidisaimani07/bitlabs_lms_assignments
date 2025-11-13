e90ea3479286d275850071f264bd40a80f4ce578
Nagul Meera Shaik <161418040+Nagul-Tekworks@users.noreply.github.com>
2025-11-13 15:33:42 +0530

    Update ApplicantNavBar.js

--- src/components/applicantcomponents/ApplicantNavBar.js
+++ src/components/applicantcomponents/ApplicantNavBar.js
@@ -308,7 +308,7 @@ function ApplicantNavBar() {
 
                           <div className="robo-card-text">
                             <p className="robo-card-para">
-                              Have questions - <span onClick={handleRedirect} style={{ fontSize: "18px", fontWeight: "1200", color: "#7E3601" }}>Ask Newton!</span>
+                              Got a doubt? - <span onClick={handleRedirect} style={{ fontSize: "18px", fontWeight: "1200", color: "#7E3601" }}>Ask Newton!</span>
                             </p>
 
                             <button
